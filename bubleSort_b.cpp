d1c9e1add3004ddc14302b1bb20de84e52e01c8d
GordionA1 <farisazka2506@gmail.com>
2026-03-05 17:49:21 +0700

    procedure untuk input

--- bubleSort_b.cpp
+++ bubleSort_b.cpp
@@ -4,3 +4,19 @@ using namespace std;
 
 int arr[20]; // deklarasi variabel global array a dengan ukuran 20
 int n; // deklarasi variabel global n untukk menyimpan banyaknya elemen pada array
+
+void input ()
+{ //procedure untuk  input
+    int d; 
+    while (true)
+    {
+        cout << "masukkan elemen pada array : "; //output ke layar
+        cin >> n;                                //input dari pengguna
+        if (n<=20)                               //jika n kurang dari satu atau satu
+            break ;                              //keluar dari looop
+        else
+        {                                        //jika n lebih dari satu
+            cout<< "\nArray dapat mempunyai maksimmal 20 elemen.\n"; //output ke layar
+        }
+    }
+}
