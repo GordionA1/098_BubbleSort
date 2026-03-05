11d0cbbc50f9097840db499ebc3e27bf4fbd0eb4
GordionA1 <farisazka2506@gmail.com>
2026-03-05 19:03:19 +0700

    mendisplaykan void dan menjalankan seluruh program dengan int main

--- bubleSort_b.cpp
+++ bubleSort_b.cpp
@@ -60,3 +60,36 @@ void bubbleSortArray()
     
 }
 
+void display ()
+{
+    cout << endl ;
+    cout << "===============" << endl;
+    cout << "Element Array Yang telah tersusun" << endl;
+    cout << "===============" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j];
+        if (j < n - 1)
+        {
+            cout << "-->";
+        }
+    }
+    cout<< endl;
+    cout << endl;
+    
+    cout << "Jumlah Pass = " << n-1 << endl;
+    cout<< endl;
+    cout << endl;
+}
+
+int main()
+{
+    input ();
+
+    bubbleSortArray();
+    display (); 
+
+    system ("pause");
+    return 0;
+}
