c09c73783ca1035f27d81204cd96e8a4edddb9fe
GordionA1 <farisazka2506@gmail.com>
2026-03-05 17:39:47 +0700

    menambahkan deklarasi variabel

--- bubleSort_b.cpp
+++ bubleSort_b.cpp
@@ -1,3 +1,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int arr[20]; // deklarasi variabel global array a dengan ukuran 20
+int n; // deklarasi variabel global n untukk menyimpan banyaknya elemen pada array
